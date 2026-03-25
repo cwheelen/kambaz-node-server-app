33be7733838ffd660dcd42b96b8f0e23763fd793
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-03-25 14:28:40 -0400

    lab5 finished

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import Hello from "./Hello.js"
 import Lab5 from "./Lab5/index.js";
 const app = express()
-Lab5(app)
+app.use(cors());
+app.use(express.json());
+Lab5(app);
 Hello(app)
 app.listen(process.env.PORT || 4000)
