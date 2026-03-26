c3136658aa48b98565feca67eea54ab9935edf03
cwheelen <113392105+cwheelen@users.noreply.github.com>
2026-03-26 19:17:56 -0400

    up to 5.4.5 with errors

---
+++ kambaz/courses/dao.js
@@ -0,0 +1,41 @@
+import { v4 as uuidv4 } from "uuid";
+
+export default function CoursesDao(db) {
+  function findAllCourses() {
+    return db.courses;
+  }
+
+  function findCoursesForEnrolledUser(userId) {
+    const { courses, enrollments } = db;
+    return courses.filter((course) =>
+      enrollments.some(
+        (e) => e.user === userId && e.course === course._id
+      )
+    );
+  }
+
+  function createCourse(course) {
+    const newCourse = { ...course, _id: uuidv4() };
+    db.courses = [...db.courses, newCourse];
+    return newCourse;
+  }
+
+  function deleteCourse(courseId) {
+    db.courses = db.courses.filter((c) => c._id !== courseId);
+    db.enrollments = db.enrollments.filter((e) => e.course !== courseId);
+  }
+
+  function updateCourse(courseId, courseUpdates) {
+    const course = db.courses.find((c) => c._id === courseId);
+    Object.assign(course, courseUpdates);
+    return course;
+  }
+
+  return {
+    findAllCourses,
+    findCoursesForEnrolledUser,
+    createCourse,
+    deleteCourse,
+    updateCourse,
+  };
+}
